b6b99772a8ffa89a94199cbc7a75b075c66a9a64
jnosek <jnosek@gmail.com>
2026-02-22 16:36:34 -0500

    added LDA Instruction and tests

---
+++ src/Platforms/Commodore64.Test/Instructions/LDA_Test.cs
@@ -0,0 +1,198 @@
+using System;
+using Dotnemulator.Platforms.Commodore64.Architecture;
+using Dotnemulator.Platforms.Commodore64.Architecture.Instruction;
+using Dotnemulator.Platforms.Commodore64.Test.Mocks;
+
+namespace Dotnemulator.Platforms.Commodore64.Test.Instructions;
+
+[TestClass]
+public class LDA_Test
+{
+ [TestMethod]
+    public void Immediate()
+    {
+        // arrange
+        var emulator = new TestEmulator([
+            LDA.BASE_OP_CODE | AddressMode.Immediate,
+            0b0000_0010,
+            0xEA
+        ]);
+
+        // act
+        emulator.Start();
+
+        // assert
+        Assert.AreEqual(0b0000_0010, emulator.Accumulator);
+        Assert.IsFalse(emulator.Cpu.P.ZeroFlag);
+        Assert.IsFalse(emulator.Cpu.P.NegativeFlag);
+    }
+
+    [TestMethod]
+    public void ZeroPage_WithZero()
+    {
+        // arrange
+        var emulator = new TestEmulator([
+            LDA.BASE_OP_CODE | AddressMode.ZeroPage,
+            0x03,
+            0xEA
+        ],
+        [ (0x3, 0b0000_0000) ]);
+
+        // act
+        emulator.Start();
+
+        // assert
+        Assert.AreEqual(0b0000_0000, emulator.Accumulator);
+        Assert.IsTrue(emulator.Cpu.P.ZeroFlag);
+        Assert.IsFalse(emulator.Cpu.P.NegativeFlag);
+    }
+
+    [TestMethod]
+    public void ZeroPageX_WithNegative()
+    {
+        // arrange
+        var emulator = new TestEmulator([
+            LDA.BASE_OP_CODE | AddressMode.ZeroPageX,
+            0x03,
+            0xEA
+        ],
+        [ (0x06, 0b1000_0010) ])
+        {
+            X = 0x03
+        };
+
+        // act
+        emulator.Start();
+
+        // assert
+        Assert.AreEqual(0b1000_0010, emulator.Accumulator);
+        Assert.IsFalse(emulator.Cpu.P.ZeroFlag);
+        Assert.IsTrue(emulator.Cpu.P.NegativeFlag);
+    }
+
+    [TestMethod]
+    public void Absolute()
+    {
+        // arrange
+        var emulator = new TestEmulator([
+            LDA.BASE_OP_CODE | AddressMode.Absolute,
+            0x00,
+            0xC0,
+            0xEA
+        ],
+        [ (0xC000, 0b0000_0010) ]);
+
+        // act
+        emulator.Start();
+
+        // assert
+        Assert.AreEqual(0b0000_0010, emulator.Accumulator);
+        Assert.IsFalse(emulator.Cpu.P.ZeroFlag);
+        Assert.IsFalse(emulator.Cpu.P.NegativeFlag);
+    }
+
+    // add a negative flag check
+    [TestMethod]
+    public void AbsoluteX()
+    {
+        // arrange
+        var emulator = new TestEmulator([
+            LDA.BASE_OP_CODE | AddressMode.AbsoluteX,
+            0x00,
+            0xC0,
+            0xEA
+        ],
+        [ (0xC003, 0b1000_0010) ])
+        {
+            X = 0x03
+        };
+
+        // act
+        emulator.Start();
+
+        // assert
+        Assert.AreEqual(0b1000_0010, emulator.Accumulator);
+        Assert.IsFalse(emulator.Cpu.P.ZeroFlag);
+        Assert.IsTrue(emulator.Cpu.P.NegativeFlag);
+    }
+
+    [TestMethod]
+    public void AbsoluteY()
+    {
+        // arrange
+        var emulator = new TestEmulator([
+            LDA.BASE_OP_CODE | AddressMode.AbsoluteY,
+            0x00,
+            0xC0,
+            0xEA
+        ],
+        [ (0xC006, 0b0000_0010) ])
+        {
+            Y = 0x06
+        };
+
+        // act
+        emulator.Start();
+
+        // assert
+        Assert.AreEqual(0b0000_0010, emulator.Accumulator);
+        Assert.IsFalse(emulator.Cpu.P.ZeroFlag);
+        Assert.IsFalse(emulator.Cpu.P.NegativeFlag);
+    }
+
+    [TestMethod]
+    public void IndexedIndirect()
+    {
+        // arrange
+        var emulator = new TestEmulator([
+            LDA.BASE_OP_CODE | AddressMode.Indexed_Indirect,
+            0x02,
+            0xEA
+        ],
+        // ram
+        [
+            (0x06, 0x06),
+            (0x07, 0xC0),
+            (0xC006, 0b0000_0010)])
+        {
+            X = 0x04
+        };
+
+        // act
+        emulator.Start();
+
+        // assert
+        Assert.AreEqual(0b0000_0010, emulator.Accumulator);
+        Assert.IsFalse(emulator.Cpu.P.ZeroFlag);
+        Assert.IsFalse(emulator.Cpu.P.NegativeFlag);
+    }
+
+    [TestMethod]
+    public void IndirectIndexed()
+    {
+        // arrange
+        var emulator = new TestEmulator(
+        // rom
+        [
+            ORA.BASE_OP_CODE | AddressMode.Indirect_Indexed,
+            0x02,
+            0xEA
+        ],
+        // ram
+        [
+            (0x02, 0x06),
+            (0x03, 0xC0),
+            (0xC00A, 0b0000_0010)])
+        {
+            Y = 0x04
+        };
+
+        // act
+        emulator.Start();
+
+        // assert
+        Assert.AreEqual(0b0000_0010, emulator.Accumulator);
+        Assert.IsFalse(emulator.Cpu.P.ZeroFlag);
+        Assert.IsFalse(emulator.Cpu.P.NegativeFlag);
+    }
+}
---
+++ src/Platforms/Commodore64/Architecture/Instruction/LDA.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Dotnemulator.Platforms.Commodore64.Architecture.Instruction;
+
+/// <summary>
+/// Load Accumulator Instruction
+/// </summary>
+/// <param name="cpu"></param>
+/// <param name="addressMode"></param>
+class LDA (MOS6510Cpu cpu, int addressMode) : 
+    AddressModeInstruction(cpu, BASE_OP_CODE, addressMode)
+{
+    public const int BASE_OP_CODE = 0xA1;
+
+    public static readonly int[] AddressModes = [
+        AddressMode.Immediate,
+        AddressMode.Absolute,
+        AddressMode.AbsoluteX,
+        AddressMode.AbsoluteY,
+        AddressMode.ZeroPage,
+        AddressMode.ZeroPageX,
+        AddressMode.Indexed_Indirect,
+        AddressMode.Indirect_Indexed,
+    ];
+
+    public readonly int[] Cycles = [4, 5, 5, 3, 4, 6, 6];
+
+    public override void Execute(int instruction)
+    {
+        var address = DecodeOperand();
+        
+        // get value at address
+        var value = CPU.Read(address);
+        CPU.A.Write(value);
+
+        // set flags
+        CPU.P.NegativeFlag = (CPU.A.Read() & 0x80) != 0;
+        CPU.P.ZeroFlag = CPU.A.Read() == 0;
+    }
+}
